8bd4d6056f014d9d0b3411f6f275d91d2714fa3b
Shailesh <faithful.shailesh@gmail.com>
2018-07-05 01:34:34 +0530

    Added Delete Script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,62 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectId} = require('mongodb');
+
+const url = 'mongodb://localhost:27017/TodoApp';
+const dbName = 'TodoApp';
+
+
+
+MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+    if(err){
+        return console.log('Unable to connect to mongo db server',err);
+    }
+    console.log('Connected to Mongo DB server');
+
+    var db = client.db(dbName);
+
+    // db.collection('Todos')
+    //     .deleteMany({text: 'Eat Lunch'})
+    //     .then((result) => {
+    //         console.log(result);
+    //     });
+
+    // db.collection('Todos')
+    //     .deleteOne({text: 'Eat Lunch'})
+    //     .then((result) => {
+    //         console.log(result);
+    //     });
+
+    // db.collection('Todos')
+    //     .findOneAndDelete({completed: false})
+    //     .then((result) => {
+    //         console.log(result);
+    //     });
+
+
+
+    // db.collection('Users')
+    //     .deleteMany({ _id: new ObjectId ("5b3cf091de6f2f134967f6ae")})
+    //     .then((result) => {
+    //         console.log(result);
+    //     });
+
+    // db.collection('Users')
+    //     .deleteOne({_id: new ObjectId('5b3cf067b39c26132cf1af86')})
+    //     .then((result) => {
+    //         console.log(result);
+    //     });
+
+    // db.collection('Users')
+    //     .findOneAndDelete({_id: new ObjectId('5b3cdac6f7d6890db8cf3a47')})
+    //     .then((result) => {
+    //         console.log(result);
+    //     });
+
+          
+    // db.close();
+});
+
+
+
+
+
