08703382fec91cf87c1547113ce2ff59a4919d15
Shailesh <faithful.shailesh@gmail.com>
2018-07-09 15:21:10 +0530

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
-var {ObjectId} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3100;
 
 app.use(bodyParser.json());
 
@@ -34,25 +34,41 @@ app.get('/todos', (req, res) => {
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
 
-  if(!ObjectId.isValid(id)){
-    return  res.status(404).send();
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
   }
 
   Todo.findById(id).then((todo) => {
-    if(!todo){ return res.status(404).send(); }
-
+    if (!todo) {
+      return res.status(404).send();
+    }
 
     res.send({todo});
-  }, (e) => {
-    res.status(400).send();
   }).catch((e) => {
     res.status(400).send();
   });
+});
 
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send(todo);
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 app.listen(port, () => {
-  console.log(`Started up at ${port}`);
+  console.log(`Started up at port ${port}`);
 });
 
 module.exports = {app};
